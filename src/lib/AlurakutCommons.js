1cf8bb2824a39a0e125a2275bdb8ebf2fe6f29fa
Alex <alexoliveira604@gmail.com>
2021-07-13 14:19:52 -0300

    aula 2

--- src/lib/AlurakutCommons.js
+++ src/lib/AlurakutCommons.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import styled, { css } from 'styled-components';
 import NextLink from 'next/link';
-
 const BASE_URL = 'http://alurakut.vercel.app/';
 const v = '1';
-
 
 function Link({ href, children, ...props }) {
   return (
